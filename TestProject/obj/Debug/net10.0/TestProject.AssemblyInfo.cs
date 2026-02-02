b3fca80ac20a7e5120938bf6e4f7987d95bce447
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-02-02 17:12:05 -0500

    function overloading

--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c847b66edb48c57e844ed52d4c119e7a462a77db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5decd8a968bfe4846a6737e7c71643fdb787a0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
