c28da43823090c463809a2f7fdf0de4f3c968d8f
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-02-02 19:04:42 -0500

    test array functions

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,21 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿/**
+string[] fraudulentOrderIDs = new string[3];
 
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
 //fraudulentOrderIDs[3] = "C789";
+
+**/
+
+string[] fraudulentOrderIDs = [ "A12", "B45", "C78", "D90" ];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4953e78ecdb0702a33062546ff0a910116e21609")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62607b7b94d31a354afffae9f04488600ed4bd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
